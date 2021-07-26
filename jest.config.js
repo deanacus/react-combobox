31fb3201ead3144210713f48e452eb06e0d2e40f
Dean Harris <dean@harris.tc>
2021-07-26 12:34:05 +1000

    chore: install jest and associated deps

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.export = {
+  setupFilesAfterEnv: ['@testing-library/jest-dom', '<rootDir>/jest-setup.js'],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
